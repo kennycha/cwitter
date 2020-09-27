f8e3a3d2b777fc72fd734533fc11aa7fcbaed12d
kennycha <dudqn136@naver.com>
2020-09-28 00:46:11 +0900

    update nav design"

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,15 +9,25 @@ const Navigation = ({ userObj }) => {
     <nav>
       <ul style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
         <li>
-          <Link to="/" style={{ marginRight: 10 }}>
+          <Link
+            to="/"
+            style={{
+              marginRight: 20,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              fontSize: 12,
+            }}
+          >
             <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
+            <span style={{ marginTop: 10 }}>Cweets</span>
           </Link>
         </li>
         <li>
           <Link
             to="/profile"
             style={{
-              marginLeft: 10,
+              marginLeft: 20,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
